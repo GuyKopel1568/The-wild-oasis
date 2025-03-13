6ca7bd76286e5b14d42281cec23b8d5b3a23b94e
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-13 11:58:33 +0200

    Load the data from the database

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import Dashboard from './pages/Dashboard';
 import PageNotFound from './pages/PageNotFound';
 import Bookings from './pages/Bookings';
