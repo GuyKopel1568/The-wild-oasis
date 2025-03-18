92bf333d9cec87bdb70a8b81f8e261fc860ba355
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-18 16:31:35 +0200

    Show the active filter

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -37,6 +37,7 @@ const FilterButton = styled.button`
 
 function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
 
   function handleClick(value) {
     searchParams.set(filterField, value);
@@ -45,8 +46,12 @@ function Filter({ filterField, options }) {
 
   return (
     <StyledFilter>
-      {options.map((option, index) => (
-        <FilterButton onClick={() => handleClick(option.value)} key={index}>
+      {options.map((option) => (
+        <FilterButton
+          onClick={() => handleClick(option.value)}
+          key={option.value}
+          active={option.value === currentFilter}
+        >
           {option.label}
         </FilterButton>
       ))}
