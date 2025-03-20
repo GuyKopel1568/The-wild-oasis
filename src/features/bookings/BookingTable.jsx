8c46570566c3558acc7a607ed42929b69b63b9ec
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-20 12:46:16 +0200

    Reusable pagination component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import { useBookings } from './useBookings';
 import Spinner from '../../ui/Spinner';
+import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
   const { bookings, isLoading } = useBookings();
@@ -13,25 +14,31 @@ function BookingTable() {
   if (!bookings.length) return <Empty resource="bookings" />;
 
   return (
-    <Menus>
-      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
-        <Table.Header>
-          <div>Cabin</div>
-          <div>Guest</div>
-          <div>Dates</div>
-          <div>Status</div>
-          <div>Amount</div>
-          <div></div>
-        </Table.Header>
+    <>
+      <Menus>
+        <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+          <Table.Header>
+            <div>Cabin</div>
+            <div>Guest</div>
+            <div>Dates</div>
+            <div>Status</div>
+            <div>Amount</div>
+            <div></div>
+          </Table.Header>
 
-        <Table.Body
-          data={bookings}
-          render={(booking) => (
-            <BookingRow key={booking.id} booking={booking} />
-          )}
-        />
-      </Table>
-    </Menus>
+          <Table.Body
+            data={bookings}
+            render={(booking) => (
+              <BookingRow key={booking.id} booking={booking} />
+            )}
+          />
+
+          <Table.Footer>
+            <Pagination count={15} />
+          </Table.Footer>
+        </Table>
+      </Menus>
+    </>
   );
 }
 
