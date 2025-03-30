b5600a83476a7d54a6333908c5d34ffb6d211d69
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-30 20:32:14 +0300

    Update avatar and user name

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateCurrentUser } from '../../services/apiAuth';
+import toast from 'react-hot-toast';
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: () => {
+      toast.success('User Account Successfully Updated!');
+      queryClient.invalidateQueries('user');
+    },
+    onError: (error) => {
+      toast.error('User could not be updated');
+      console.error(error);
+    },
+  });
+
+  return { updateUser, isUpdating };
+}
