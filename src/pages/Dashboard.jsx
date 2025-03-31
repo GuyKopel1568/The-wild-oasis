805358b0456e5b931fc734487de0c0497b100f8a
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-31 12:01:33 +0300

    Computing recent stays and bookings

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,19 @@
 import DashboardFilter from '../features/dashboard/DashboardFilter';
 import DashboardLayout from '../features/dashboard/DashboardLayout';
+import { useRecentBookings } from '../features/dashboard/useRecentBookings';
+import { useRecentStays } from '../features/dashboard/useRecentStays';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
+import Spinner from '../ui/Spinner';
 
 function Dashboard() {
+  const { isLoading: isLoadingtBooking, bookings } = useRecentBookings();
+  const { isLoading: isLoadingtSays, stays, confirmedStays } = useRecentStays();
+
+  if (isLoadingtBooking || isLoadingtSays) return <Spinner />;
+  console.log(bookings);
+  console.log(confirmedStays);
+
   return (
     <>
       <Row type="horizontal">
