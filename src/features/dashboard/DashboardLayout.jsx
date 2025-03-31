685fe085b7d481dc60622eb9b624f97c34c66c0f
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-31 16:22:33 +0300

    Displaying pie chart of guests duration

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useCabins } from '../cabins/useCabins';
 import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -37,6 +38,7 @@ function DashboardLayout() {
       />
       <div>Today&apos;s activities </div>
       <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
